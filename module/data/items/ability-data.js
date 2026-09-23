fa7e8a597663d60808badf8b7f65eb659edadc87
Caleb Prime <primec@oregonstate.edu>
2026-09-23 12:09:40 -0700

    Revert "Add psionics system, psionic abilities pack, and 1e character classes (Criminal, Robot, Ranger, Retiree, Psychic, Emissary)"
    This reverts commit 909a759aa41ac4f84ef413bd09042695eaf02b8a.

--- module/data/items/ability-data.js
+++ module/data/items/ability-data.js
@@ -5,13 +5,8 @@ export class AbilityData extends foundry.abstract.TypeDataModel {
     const fields = foundry.data.fields;
     return {
       ...sharedItemFields(fields),
-      text:    new fields.StringField({ initial: "An Ability" }),
-      roll:    new fields.StringField({ initial: "" }),
-      psionic: new fields.BooleanField({ initial: false }),
-      level:   new fields.NumberField({ initial: 1, min: 1, max: 3 }),
-      level_1: new fields.HTMLField({ initial: "" }),
-      level_2: new fields.HTMLField({ initial: "" }),
-      level_3: new fields.HTMLField({ initial: "" }),
+      text: new fields.StringField({ initial: "An Ability" }),
+      roll: new fields.StringField({ initial: "" }),
     };
   }
 }
--- module/data/items/class-data.js
+++ module/data/items/class-data.js
@@ -24,11 +24,8 @@ export class ClassData extends foundry.abstract.TypeDataModel {
         skills_granted: new fields.ArrayField(new fields.StringField()),
       }),
 
-      // choose_stat: Array<{modification: number, stats: string[]}>
-      // choose_skill_or: Array<Array<{name, trained, expert, ..., from_list: string[]}>>
-      // Both use ObjectField elements because their contents are complex/nested objects.
       selected_adjustment: new fields.SchemaField({
-        choose_stat: new fields.ArrayField(new fields.ObjectField()),
+        choose_stat: new fields.ArrayField(new fields.StringField()),
         choose_skill_and: new fields.SchemaField({
           trained:          new fields.NumberField({ initial: 0 }),
           expert:           new fields.NumberField({ initial: 0 }),
@@ -36,7 +33,7 @@ export class ClassData extends foundry.abstract.TypeDataModel {
           master:           new fields.NumberField({ initial: 0 }),
           master_full_set:  new fields.NumberField({ initial: 0 }),
         }),
-        choose_skill_or: new fields.ArrayField(new fields.ObjectField()),
+        choose_skill_or: new fields.ArrayField(new fields.StringField()),
       }),
 
       roll_tables: new fields.SchemaField({
